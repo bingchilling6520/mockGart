daa727bf3797ac8eb7530fe864192d7d6e660d06
KhiemGOM <khiem06072007@gmail.com>
2022-12-17 23:21:20 +0700

    17/12/2022 push the right save files

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,6 +5,12 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.AutoDriveStraight;
+import frc.robot.commands.AutoEatBalls;
+import static frc.robot.SubsystemInstance.*;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -16,24 +22,31 @@ import edu.wpi.first.wpilibj.Joystick;
  */
 public final class Constants 
 {
-      public static final double NORMSPEED = 0.2, FASTSPEED = 0.8, INTAKESPEED = 1;
-      
+      public static final double NORMSPEED = 0.2, FASTSPEED= 0.8, INTAKESPEED=1;
       public final class buttonID
       {
-            public static final int YAXISLEFT = 1, YAXISRIGHT = 5, RIGHTTRIGGER = 3, INTAKEBUTTON = 1;
+      public static final int YAXISLEFT = 1, YAXISRIGHT=5, RIGHTTRIGGER= 3, INTAKEBUTTON = 1;
       }
-      
       public final class talonID
       {
             public static final int RIGHTMASTERID = 1, RIGHTFOLLOWID = 2,
                                     LEFTMASTERID = 3, LEFTFOLLOWID = 4,
                                     INTAKEID = 5;
       }
-      
       public final class PID
       {
             public static final double KI = 0, KP = 1.0/18, KD = 0, KTOLERANCE = 2.0, KTOLERANCEVELOCITY = 0.8 ;
       }
-      
       public final static Joystick JOYSTICK = new Joystick(0);
+      public final static class autoCMD 
+      {
+            // Drive Straight quickly for 0.5s then move slowly while eating balls in 0.8s
+            public static final Command TESTCMD = 
+            new SequentialCommandGroup(
+                  new AutoDriveStraight(m_driverBaseSubsystem,FASTSPEED).withTimeout(0.5),
+                  new ParallelCommandGroup(
+                        new AutoDriveStraight(m_driverBaseSubsystem, NORMSPEED),
+                        new AutoEatBalls(m_intakeSubsystem,INTAKESPEED)
+                  ).withTimeout(0.8));
+      }
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,22 +1,20 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 //import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 //import frc.robot.commands.DriveStraight;
-import frc.robot.subsystems.DriverBaseSubsys;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import static frc.robot.SubsystemInstance.*;
 //import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import edu.wpi.first.wpilibj2.command.button.Trigger;   
-import static frc.robot.Constants.*;          
+import static frc.robot.Constants.*;   
+import static frc.robot.Constants.autoCMD.*;       
 import frc.robot.commands.DriverBaseCmd;   
-import frc.robot.subsystems.IntakeSubsys;
 import static frc.robot.Constants.buttonID.*;
-import frc.robot.commands.AutoDriveStraight;
-import frc.robot.commands.AutoEatBalls;
+//import frc.robot.commands.AutoDriveStraight;
+//import frc.robot.commands.AutoEatBalls;
 import frc.robot.commands.IntakeCmd;                                            
 
 /**
@@ -27,9 +25,7 @@ import frc.robot.commands.IntakeCmd;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final DriverBaseSubsys m_driverBaseSubsystem = new DriverBaseSubsys();
   private final DriverBaseCmd m_driverBaseCommand = new DriverBaseCmd(m_driverBaseSubsystem);
-  private final IntakeSubsys m_intakeSubsystem = new IntakeSubsys();
   private final IntakeCmd m_intakeCommand = new IntakeCmd(m_intakeSubsystem);
   
   private final JoystickButton buttonA = new JoystickButton(JOYSTICK, INTAKEBUTTON);
@@ -58,13 +54,6 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-   // Drive Straight quickly for 0.5s then move slowly while eating balls in 0.8s
-   return new SequentialCommandGroup(
-    new AutoDriveStraight(m_driverBaseSubsystem,FASTSPEED).withTimeout(0.5),
-    new ParallelCommandGroup(
-      new AutoDriveStraight(m_driverBaseSubsystem, NORMSPEED),
-      new AutoEatBalls(m_intakeSubsystem,INTAKESPEED)
-    ).withTimeout(0.8));
+   return TESTCMD;
   }
 }
