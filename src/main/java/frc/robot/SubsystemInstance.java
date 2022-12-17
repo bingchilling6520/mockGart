946ec9781e5981c0b469f19b8a8e0924e44cc68d
KhiemGOM <khiem06072007@gmail.com>
2022-12-17 23:12:54 +0700

    test

--- src/main/java/frc/robot/SubsystemInstance.java
+++ src/main/java/frc/robot/SubsystemInstance.java
@@ -4,5 +4,16 @@
 
 package frc.robot;
 
-/** Add your docs here. */
-public class SubsystemInstance {}
+import frc.robot.subsystems.DriverBaseSubsys;
+import frc.robot.subsystems.GyroSubsys;
+import frc.robot.subsystems.IntakeSubsys;
+import frc.robot.subsystems.PIDControllerSubsys;
+
+/** A class to acess the subsystem. */
+public final class SubsystemInstance {
+      public static final DriverBaseSubsys m_driverBaseSubsystem = new DriverBaseSubsys();
+      public static final IntakeSubsys m_intakeSubsystem = new IntakeSubsys();
+      public static final PIDControllerSubsys m_PIDSubsystem = new PIDControllerSubsys();
+      public static final GyroSubsys m_GyroSubsystem = new GyroSubsys();
+
+}
