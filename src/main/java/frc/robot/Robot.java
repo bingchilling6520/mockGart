8d842e8918cfd2f8e0ee8ccdc76d044515f8558e
KhiemGOM <khiem06072007@gmail.com>
2022-12-28 00:01:01 +0700

    28/12/2022 Quick fix for prev commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import static frc.robot.Constants.*;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.Algorithm.RunningMode;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -49,7 +51,10 @@ public class Robot extends TimedRobot {
 
   /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    
+    MODE = RunningMode.disabled;
+  }
 
   @Override
   public void disabledPeriodic() {}
@@ -57,6 +62,8 @@ public class Robot extends TimedRobot {
   /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
+    
+    MODE = RunningMode.auto;
     CommandScheduler.getInstance().cancelAll();
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
@@ -79,6 +86,7 @@ public class Robot extends TimedRobot {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
+    MODE = RunningMode.teleOperated;
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
