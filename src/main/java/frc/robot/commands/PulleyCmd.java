b0b58333e23a09227036ce06cf97b0b4d6aa788d
giangvinhloc610 <giangvinhloc610@gmail.com>
2023-01-03 19:19:34 +0700

    Uncomment stuff

--- src/main/java/frc/robot/commands/PulleyCmd.java
+++ src/main/java/frc/robot/commands/PulleyCmd.java
@@ -29,8 +29,8 @@ public class PulleyCmd extends CommandBase {
   public void execute() {
     double speed = (JOYSTICK2.getRawAxis(BOOSTPULLEY) > SENSITIVITY ?
                       SmartDashboard.getNumber("Pulley Fast Speed", PULLEYFASTSPEED) :
-                      SmartDashboard.getNumber("Pulley Slow Speed", PULLEYSLOWSPEED));
-                     // * (JOYSTICK2.getRawAxis(INVERT) > SENSITIVITY ? -1 : 1);
+                      SmartDashboard.getNumber("Pulley Slow Speed", PULLEYSLOWSPEED))
+                     * (JOYSTICK2.getRawAxis(INVERT) > SENSITIVITY ? -1 : 1);
     m_pulley.pull(speed, speed);
   }
 
